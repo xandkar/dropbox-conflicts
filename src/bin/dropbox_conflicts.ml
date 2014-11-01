7f1fcd48db0e7a03495222d9f6e43c19646c450e
Siraaj Khandkar <siraaj@khandkar.net>
2014-11-01 14:32:39 -0400

    Save the conflicts graph before printing it.

--- src/bin/dropbox_conflicts.ml
+++ src/bin/dropbox_conflicts.ml
@@ -1,6 +1,49 @@
 open Core.Std
 open Async.Std
 
+module DiGraph : sig
+  type node_id = string
+
+  type t
+
+  val create : unit -> t
+
+  val add_link : t -> src:node_id -> dst:node_id -> unit
+
+  val print_dot : t -> unit
+end = struct
+  type node_id = string
+
+  type links =
+    { incoming : node_id Hash_set.t
+    ; outgoing : node_id Hash_set.t
+    }
+
+  type t =
+    (node_id, links) Hashtbl.t
+
+  let create () =
+    String.Table.create ()
+
+  let add_link t ~src ~dst =
+    let get_links node_id =
+      Hashtbl.find_or_add t node_id ~default:(fun () ->
+        { incoming = String.Hash_set.create ()
+        ; outgoing = String.Hash_set.create ()
+        }
+      )
+    in
+    Hash_set.add (get_links src).outgoing dst;
+    Hash_set.add (get_links dst).incoming src
+
+  let print_dot t =
+    print_endline "digraph G {";
+    Hashtbl.iter t ~f:(fun ~key:src ~data:{outgoing=dsts; _} ->
+      Hash_set.iter dsts ~f:(fun dst -> printf "%S -> %S;\n" src dst)
+    );
+    print_endline "}"
+end
+
 let main ~dir =
   let paths_r     , paths_w     = Pipe.create () in
   let conflicts_r , conflicts_w = Pipe.create () in
@@ -24,13 +67,13 @@ let main ~dir =
     Pipe.close conflicts_w
   in
   let worker_printer () =
-    print_endline "digraph G {";
+    let graph = DiGraph.create () in
     Pipe.iter conflicts_r ~f:(
-      fun {Dropbox_conflict.path_current; path_original; _} ->
-        printf "%S -> %S;\n" path_original path_current;
+      fun {Dropbox_conflict.path_current=dst; path_original=src; _} ->
+        DiGraph.add_link graph ~src ~dst;
         return ()
     ) >>| fun () ->
-    print_endline "}"
+    DiGraph.print_dot graph
   in
   Deferred.List.iter
     ~how:`Parallel
